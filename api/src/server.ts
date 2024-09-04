6d829b0bd0ec71dd26de7f261344868f7994f3b0
strahd <jv.palha@gmail.com>
2024-09-04 19:17:55 -0300

    fix: Markdown

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,5 +1,24 @@
 import { app } from "./app";
+import os from 'node:os';
 
-app.listen(4000, ()=>{
-    console.log("Server online!")
+function getLocalIp() {
+    const networkInterfaces = os.networkInterfaces();
+    for (const interfaceName in networkInterfaces) {
+        const interfaces = networkInterfaces[interfaceName];
+        if (interfaces) {
+            for (const iface of interfaces) {
+                if (iface.family === 'IPv4' && !iface.internal) {
+                    return iface.address;
+                }
+            }
+        }
+    }
+    return '127.0.0.1'; // Fallback para localhost se o IP não for encontrado
+}
+
+app.listen({
+    port: 4000,
+}, ()=>{
+    console.log(`Server online at http://${getLocalIp()}:4000`);
+    console.log("Please, verify with the frontend are using the same IP address at 'drive-front/src/lib/axios.ts'");
 })
--- drive-front/src/lib/axios.ts
+++ drive-front/src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:4000/"
+    baseURL: "http://127.0.0.1:4000/"
 })
 
